823732d60878b7de1a1ef895e3a1af700771725b
siddheshsurve <sysurve24@gmail.com>
2023-07-11 19:41:06 +0530

    Done with routes

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -5,9 +5,12 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-    res.status(200).json({message : "Get all contacts"});
-})
+// app.get("/api/contacts", (req, res) => {
+//     res.status(200).json({message : "Get all contacts"});
+// })
+
+//adding middleware
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
