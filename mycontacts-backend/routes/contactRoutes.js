ed5c70e98f01697e19dc5f5a7006237620a40bfe
siddheshsurve <sysurve24@gmail.com>
2023-07-11 20:03:43 +0530

    controllers as middleware for logic

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,24 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { getContacts, createContact, getContact, updateContact, deleteContact } = require("../controllers/contactController")
 
-router.route("/").get((req, res) => {
-    res.status(200).json({ message : "Get all contacts " });
-});
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/:id").get((req, res) => {
-    res.status(200).json({ message : `Get contact for ${req.params.id}` });
-});
-
-router.route("/").post((req, res) => {
-    res.status(200).json({ message : "Create new contact" });
-});
-
-router.route("/:id").put((req, res) => {
-    res.status(200).json({ message : `Update contact for ${req.params.id}` });
-});
-
-router.route("/:id").delete((req, res) => {
-    res.status(200).json({ message: `Delete cotact for ${req.params.id}` });
-});
+router.route("/:id").get(getContact).put(deleteContact).delete(updateContact);
 
 module.exports = router;
