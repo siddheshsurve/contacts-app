645e2cf1a82584fdfb0233a0b7e8ce0571f9c982
siddheshsurve <sysurve24@gmail.com>
2023-07-11 20:33:06 +0530

    created errorHandling middleware

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { errorHandler } = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -8,8 +9,11 @@ const port = process.env.PORT || 5000;
 //middleware : parser to parse body
 app.use(express.json());
 
-//adding middleware
+//adding routing middleware
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+//middleware : error handling
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
