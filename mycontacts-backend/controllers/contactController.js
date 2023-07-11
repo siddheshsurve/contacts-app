005c726bd67f282e273595bbbeb60593e175a482
siddheshsurve <sysurve24@gmail.com>
2023-07-11 20:15:54 +0530

    tested error handling for empty body

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -10,6 +10,14 @@ const getContacts = (req, res) => {
 //@access public
 const createContact = (req, res) => {
     console.log("The request body is : ", req.body);
+
+    //error handling : empty body
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory !");
+    }
+
     res.status(201).json( {message : "Contact Created"} );
 }
 
