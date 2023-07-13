ad554907a97a530e4d7c82fad00ef9f0248de162
siddheshsurve <sysurve24@gmail.com>
2023-07-13 22:11:48 +0530

    Added user routes : Registration, Login & Current

---
+++ mycontacts-backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+    res.json({ message : "Register the user" });
+});
+
+router.post("/login", (req, res) => {
+    res.json({ message : "Login user" });
+});
+
+router.get("/current", (req, res) => {
+    res.json({ message : "Current user information" });
+});
+
+module.exports = router;
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 //adding routing middleware
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 //middleware : error handling
 app.use(errorHandler);
