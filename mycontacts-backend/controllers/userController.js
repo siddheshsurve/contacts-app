cd1c6a73d261c7e5671f6533aea06c115f8c18e0
siddheshsurve <sysurve24@gmail.com>
2023-07-15 19:07:33 +0530

    init user registration

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,9 +1,25 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async(req, res) => {
+    const [ username, email, password ] = req.body;
+    if(!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    //if user (email) is already exist in db
+    const userAvailable = await User.findOne({ email });
+    if(userAvailable) {
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+
+    
+
     res.json({ message : "Register the user" });
 });
 
