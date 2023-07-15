46c75c13179b3fc6724ab536cbab0b4d9fec2f1c
siddheshsurve <sysurve24@gmail.com>
2023-07-15 19:47:02 +0530

    User Login : JWT accessToken

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -50,7 +51,38 @@ const registerUser = asyncHandler(async(req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async(req, res) => {
-    res.json({ message : "Login the user" });
+    
+    const { email, password } = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+    const user = await User.findOne({ email });
+
+    //compare user provided password with db hashed password
+    if(user && (await bcrypt.compare(password, user.password))) {
+        
+        const accessToken = jwt.sign({
+            user : {
+                username : user.username,
+                email : user.email,
+                id : user.id,
+            },
+        }, 
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "1m",
+        }
+        );
+
+        res.status(200).json({ accessToken });
+    }
+    else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
+
 });
 
 //@desc Current User
